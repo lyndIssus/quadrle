c46a46bddfeef8f9640fe2dd76b648938f708a6d
lyndIssus <167985078+lyndIssus@users.noreply.github.com>
2024-04-24 16:49:19 -0300

    sistema random+ trocar imagem

--- aula.dart
+++ aula.dart
@@ -1,26 +1,22 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:primeirojeto/aula.dart';
 
+class telaMain extends StatefulWidget {
+  const telaMain({ super.key });
 
-main() {
-  runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-    
-      home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.blueGrey, title: Text("QUADRLE"),centerTitle: true),
-        body: MyLayout()),
-    );
-  }
+  State<telaMain> createState() => _TelaMainState();
 }
-
-
-class MyLayout extends StatelessWidget {
-
-
+var pontuacao = 0;
+var imgAtual = 0;
+var answers = [["unifor","Unifor"],["nice","Nice"]];
+String? text; 
+class _TelaMainState extends State<telaMain> {
+  List<AssetImage> assetImages = [
+    const AssetImage('assets/images/ead-unifor-novo.png'),
+    const AssetImage("assets/images/monalsa.jpg")
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,53 +26,40 @@ class MyLayout extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.all(23.0),
             child: Column( mainAxisAlignment: MainAxisAlignment.center,
-            children: [ 
-              ElevatedButton.icon(onPressed: (){
-                
-                Navigator.push(context,MaterialPageRoute(builder: (context)=> telaMain()));
-              }, icon: Icon(Icons.play_arrow) ,label: Text("Jogar"), ),
-              OutlinedButton.icon(onPressed: (){
-                testAlert(context);
-              }
-                  ,label: Text("Como Jogar"),icon: Icon(Icons.question_mark),)
+            children: [ Text("Pontuação: ${pontuacao}" ),SizedBox(child:
+              SizedBox(width: 800,height: 200,child: Image(image: assetImages[imgAtual],alignment: Alignment.topCenter,),
+              )
+              ),SizedBox(width: 500,child:TextField(onChanged:(value) {
+                text = value;
+              },decoration: InputDecoration(labelText: "Resposta",border: OutlineInputBorder()), )),
+              OutlinedButton(onPressed: (){
+                  if(!verify()){
+                    print("Errou");
+                  }else{
+                    
+                    setState(() {
+                      imgAtual = Random().nextInt(assetImages.length);
+                      pontuacao+=1;
+                      if(imgAtual==  assetImages.length){
+                        imgAtual = 0;
+                      }
+                    });
+                  }
+              }, child:  Text("Enviar"))
 
 
               
-                
-                
-
-              
+            
             ],
           ),
           )
         ),
       ),
     );
+  }}
+  bool verify(){
+    
+    if(answers[imgAtual].contains(text)){
+      return true;
+    }return false;
   }
-}
-void testAlert(BuildContext context) {
-  Widget okButton = TextButton(
-    child: Text("OK"),
-    onPressed: () {Navigator.pop(context);},
-  );
-      AlertDialog alert = AlertDialog(
-    title: Text("Como Jogar?"),
-    content: Text("Você será mostrado parte de diferentes obras presentes no espaço cultural, seu trabalho é encontrá-las e inserir seus titulos na caixa de texto abaixo"),
-    actions: [
-      okButton,
-    ],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
-  
-
-
-
-
