b069ccd7ab34e7e6fcc11e4875ad6cec978d7335
lyndIssus <167985078+lyndIssus@users.noreply.github.com>
2024-06-12 09:27:45 -0300

    Update fimjogo.dart

--- fimjogo.dart
+++ fimjogo.dart
@@ -1,41 +1,60 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:quadrle/main.dart';
+import 'main.dart';
 
-class ScreenTwo extends StatelessWidget {
-  
-  final List data;
-  ScreenTwo({ required this.data});
+class EndScreen extends StatelessWidget {
+  final List<int> data;
+
+  EndScreen({required this.data});
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    
-      home:Scaffold(
-        
-        body: Container(decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
-            Color.fromARGB(171, 6, 201, 255),
-            Color.fromARGB(155, 200, 59, 225)])
-        ) ,child: 
-        Center(
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(171, 6, 201, 255),
+              Color.fromARGB(155, 200, 59, 225),
+            ],
+          ),
+        ),
+        child: Center(
           child: Padding(
-            padding: EdgeInsets.all(23.0),
+            padding: const EdgeInsets.all(23.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-            children: [ Text("FIM DE JOGO",style: TextStyle(fontSize: 70),),
-                  Text("Pontuacao: ${data[0]}",style: TextStyle(fontSize: 50)),
-                  Text("Erros: ${data[1]}",style: TextStyle(fontSize: 50,),)
-              ,ElevatedButton.icon(onPressed: (){
-                
-                Navigator.push(context,MaterialPageRoute(builder: (context)=> MyApp()));
-              }, icon: Icon(Icons.keyboard_return) ,label: Text("Voltar ao Menu"), )]
-            )
-            )
-            )
-            )
-            )
+              children: [
+                Text(
+                  "FIM DE JOGO",
+                  style: TextStyle(fontSize: 70),
+                ),
+                Text(
+                  "Pontuação: ${data[0]}",
+                  style: TextStyle(fontSize: 50),
+                ),
+                Text(
+                  "Erros: ${data[1]}",
+                  style: TextStyle(fontSize: 50),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyApp()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  icon: Icon(Icons.keyboard_return),
+                  label: Text("Voltar ao Menu"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
-
-  
 }
